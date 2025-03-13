09266cfa2375ac35e8b9e96697a2175a6c53330a
Sy15886430806 <Sy15886430806@163.com>
2025-03-13 16:25:58 +0800

    SUYU

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("小明", 18);
+        System.out.println(student);
+    }
+}
